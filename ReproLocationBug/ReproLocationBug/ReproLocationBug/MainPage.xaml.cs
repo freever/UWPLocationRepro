fc7a0537765bbec07a0295f50f48a44680ed372d
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-10-02 15:03:59 +0200

    reproduced issue

---
+++ ReproLocationBug/ReproLocationBug/ReproLocationBug/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ReproLocationBug
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            
+            try
+            {
+                var location = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Best));
+            }
+            catch (Exception e)
+            {
+                //works on Android (requests permission)
+                //works on iOS (requests permission)
+                //throws exception on UWP : "LocationWhenInUse was not granted."
+
+                Console.WriteLine(e);
+                throw;
+            }
+
+            base.OnAppearing();
+        }
+    }
+}
